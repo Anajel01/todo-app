857a1ee1546fac95c0038b365df0488050a5a0bb
Anajel01 <150655411+Anajel01@users.noreply.github.com>
2024-11-05 14:49:57 +0100

    Added calendar and made todo prettier

--- BdeHandin/src/main.ts
+++ BdeHandin/src/main.ts
@@ -1,34 +1,37 @@
 import './style.css'
 
-interface Todo{
-  id:number;
-  title: string;
-  completed: boolean;
+// Define the structure of a todo item
+interface Todo {
+  id: number; // Unique identifier for each todo
+  title: string; // The text of the todo
+  completed: boolean; // Whether the todo is completed or not
+  dueDate?: string; // Optional due date for the todo
 }
 
-//step 2: initialize the todos array
-let todos: Todo[] = []
+// Step 2: Initialize the todos array
+let todos: Todo[] = [];
 
-//step 3: get refrence to the html elements, these are refrencing to the html
-const todoInput = document.getElementById('todo-input') as HTMLInputElement
-const todoList = document.getElementById('todo-list') as HTMLUListElement
-const todoForm = document.querySelector('.todo-form') as HTMLFormElement
+// Step 3: Get references to the HTML elements, these are referencing to the HTML
+const todoInput = document.getElementById('todo-input') as HTMLInputElement;
+const todoList = document.getElementById('todo-list') as HTMLUListElement;
+const todoForm = document.querySelector('.todo-form') as HTMLFormElement;
 
-//step 4: function to add a new todo
+// Step 4: Function to add a new todo
 // const addTodo = () => {} this is the basic structure of the code, always type that out before the code
-//void means there is no returns. String means they are pushing out words, can not push out numbers. 
-const addTodo = (text:string): void => { //void means there is no returns. String means they are pushing out words, can not push out numbers. 
-  const newTodo: Todo = {          //this is like a variable
+// void means there is no returns. String means they are pushing out words, cannot push out numbers. 
+const addTodo = (text: string, dueDate: string): void => { // void means there is no returns. String means they are pushing out words, cannot push out numbers. 
+  const newTodo: Todo = {          // this is like a variable
     id: Date.now(),
     title: text,
-    completed: false
-  }
-  todos.push(newTodo) //we refrence the Todo from line 21, we only use command push for arrays
-  console.log("Todo added: ", todos); //console log to test if stuff works. 
-renderTodos() //renders the todos when new is added to the list, aka you dont need to refresh the page for it to show up
-}
+    completed: false,
+    dueDate // Add the due date to the new todo
+  };
+  todos.push(newTodo); // we reference the Todo from line 21, we only use command push for arrays
+  console.log("Todo added: ", todos); // console log to test if stuff works. 
+  renderTodos(); // renders the todos when new is added to the list, aka you don't need to refresh the page for it to show up
+};
 
-
+// Function to render todos on the page
 const renderTodos = (): void => { // void because no return - what we are doing is updating the DOM
   // Clear the current list
   todoList.innerHTML = '';
@@ -37,20 +40,17 @@ const renderTodos = (): void => { // void because no return - what we are doing 
   todos.forEach(todo => { // In this specific case, .forEach is more suitable because we are directly modifying the DOM for each todo item.
     const li = document.createElement('li');
     li.className = 'todo-item'; // Add a class to the list item
-    
+
     // Use template literals to create the HTML content for each list item
     li.innerHTML = `                      
       <span>${todo.title}</span>
+      ${todo.dueDate ? `<span class="due-date">Due: ${todo.dueDate}</span>` : ''}
       <input type="checkbox" ${todo.completed ? 'checked' : ''} class="complete-checkbox" id="checkbox"> Mark as completed
       <button>Remove</button>
       <button id="editBtn">Edit</button>
-
-
-
-
     `; 
     if (todo.completed) {
-      li.style.backgroundColor = 'green';
+      li.style.backgroundColor = '#D1AD91';
     }
     // addRemoveButtonListener is further down in the code. We have onclick in the function instead of template literals. More safe to use addEventListener.
     addRemoveButtonListener(li, todo.id); // Add event listener to the remove button. li is the parent element, and todo.id is the ID of the todo.
@@ -60,12 +60,15 @@ const renderTodos = (): void => { // void because no return - what we are doing 
   });
 };
 
-renderTodos()
+renderTodos();
+
+// Function to add checkbox listener
 const addCheckboxListener = (li: HTMLLIElement, id: number) => {
   const checkbox = li.querySelector('.complete-checkbox') as HTMLInputElement;
   checkbox.addEventListener('change', () => toggleCompleteStatus(id));
 };
 
+// Function to toggle the completed status of a todo
 const toggleCompleteStatus = (id: number): void => {
   const todo = todos.find(todo => todo.id === id);
   if (todo) {
@@ -74,71 +77,72 @@ const toggleCompleteStatus = (id: number): void => {
   }
 };
 
-todoForm.addEventListener('submit', (e) => {    //e in here stands for an event, this could for example be a klick or anything other that a user might do.
-  e.preventDefault()     //this function prevents the page from reloading every time you press submit. basically removes the "deafult" function of reloading the page every time you submit something
-  const text = todoInput.value.trim()
-  if (text !=='') (
-    addTodo(text)
-  )          //this value converts strings into numbers, it takes whatever you put into it into a number
-    
+// Event listener for the form submission
+todoForm.addEventListener('submit', (e) => {    // e in here stands for an event, this could for example be a click or anything other that a user might do.
+  e.preventDefault();     // this function prevents the page from reloading every time you press submit. basically removes the "default" function of reloading the page every time you submit something
+  const text = todoInput.value.trim();
+  const dueDate = (document.getElementById('todo-due-date') as HTMLInputElement).value; // Get due date from the input
+  if (text !== '') {
+    addTodo(text, dueDate); // Pass the due date to the addTodo function
+  }        
+});
 
-
-     
-})
-
+// Function to add remove button listener
 const addRemoveButtonListener = (li: HTMLLIElement, id: number) => {
-  const removeButton = li.querySelector('button')
- removeButton?.addEventListener('click', () => removeTodo(id))   //we are listening for a klick on the button, the remove button has a question mark is optional, so that it doesnt automatically do it, it does it only when we klick, which is when the optional aspect of it becomes mandatory
-}
+  const removeButton = li.querySelector('button');
+  removeButton?.addEventListener('click', () => removeTodo(id));   // we are listening for a click on the button, the remove button has a question mark is optional, so that it doesn't automatically do it, it does it only when we click, which is when the optional aspect of it becomes mandatory
+};
 
-const removeTodo = (id:number) => {
- todos = todos.filter(todo => todo.id !== id)
- renderTodos()
-}
+// Function to remove a todo
+const removeTodo = (id: number) => {
+  todos = todos.filter(todo => todo.id !== id);
+  renderTodos();
+};
 
+// Edit function to the code, to change value in a field we added, we are coding the prompt in 
+// what we need to make this work is: access to id, the edit button, event listener, function edit, prompt, render (somewhere to type in the change) 
+const addEditButtonListener = (li: HTMLLIElement, id: number) => {
+  const editButton = li.querySelector('#editBtn');  //#editBtn is an id, we need to add it because otherwise the command will look for other buttons, it will be referenced on line 45
+  editButton?.addEventListener('click', () => editTodo(id));  // this only listens for the user, listening for the click of the specific button, it doesn't do anything else
+};
 
-//edit function to the code, to change value in a field we added, we are coding the prompt in 
-//what we need to make this work is: access to id, the edit button, event listener, function edit, prompt, render(somewhere to type in the change) 
-const addEditButtonListener = (li: HTMLLIElement,  id:number) => {
-  const editButton = li.querySelector('#editBtn')  //#editBtn is an id, we need to add it because otherwose the command will look for other buttons, it will be refrenced on line 45
-  editButton?.addEventListener('click', () => editTodo(id))  //this onlt listens for the user, listening for the klick of the specific button, it doesnt do anything else
-
-}
-
-const editTodo = (id:number) => {
-  const todo = todos.find(todo => todo.id === id) //=== if you put three of these, they are absolutelly ytue, but if we have ! we are changing if its true. = can change, == is kinda true, ===is absolitelly true
-  if (todo) {   //if the button todo is there do the action
-    const text = prompt("edit todo text", todo.title)   //check step 4 for refrence to text and title
+// Function to edit a todo
+const editTodo = (id: number) => {
+  const todo = todos.find(todo => todo.id === id); // === if you put three of these, they are absolutely true, but if we have ! we are changing if it's true. = can change, == is kinda true, === is absolutely true
+  if (todo) {   // if the button todo is there do the action
+    const text = prompt("Edit todo text", todo.title);   // check step 4 for reference to text and title
     if (text) {
-      todo.title = text
-      renderTodos()
+      todo.title = text;
+      renderTodos();
     }
   }
-}
+};
 
-//We will create an option so that our colour picker changes the colour of our background
-//for that, we need to add: function, event listener,, BGC + BODY
+// We will create an option so that our colour picker changes the colour of our background
+// for that, we need to add: function, event listener, BGC + BODY
 
 const initializeColorPicker = (): void => {
-  const colorPicker = document.getElementById('colorPicker') as HTMLInputElement
-  if(colorPicker) {
-    colorPicker.addEventListener('input', (event: Event)=> {
+  const colorPicker = document.getElementById('colorPicker') as HTMLInputElement;
+  if (colorPicker) {
+    colorPicker.addEventListener('input', (event: Event) => {
       const target = event.target as HTMLInputElement;
-      changeBackgroundColor(target.value)
-    })
+      changeBackgroundColor(target.value);
+    });
+  } else {
+    console.error('Color picker element not found');
   }
-  else {
-    console.error('Color picker element not found')
-  }
+};
 
-}
-const changeBackgroundColor = (color:string):void => {
-  document.body.style.backgroundColor = color   //command document goes to our html, that is why we need to have it, to find the code
-}
+// Function to change the background color
+const changeBackgroundColor = (color: string): void => {
+  document.body.style.backgroundColor = color;   // command document goes to our HTML, that is why we need to have it, to find the code
+};
 
+// Initialize the color picker when the DOM is fully loaded
 document.addEventListener('DOMContentLoaded', () => {     
-  initializeColorPicker()
-})
+  initializeColorPicker();
+});
+
 
 //MY TASK: okay so i need to create a button to mark my tasks
 
